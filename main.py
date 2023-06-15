1477d248caa7d66c16fdac4eb1c026919efd3864
Al.exe <77492646+AlexeyLepov@users.noreply.github.com>
2023-06-15 20:57:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,151 @@
-def custom_bracket_form(expr):
-    stack = []
-    last_char = ''
-    for i, ch in enumerate(expr):
-        if ch in {'(', '['}:
-            if last_char == ch:
-                print(f"Error: Two or more consecutive '{ch}' at position {i}")
-                return False # Two or more parentheses of the same kind in a row
-            stack.append(ch)
-        elif ch in {')', ']'}:
-            if not stack:
-                print(f"Error: Extra '{ch}' at position {i}")
-                return False # Extra closing parenthesis
-            if (ch == ')' and stack[-1] != '(') or (ch == ']' and stack[-1] != '['):
-                print(f"Error: Mismatched '{stack[-1]}' and '{ch}' at position {i}")
-                return False # Mismatched closing parenthesis
-            stack.pop()
-        elif last_char.isalpha() and ch.isalpha():
-            print(f"Error: One letter cannot be framed by brackets at position {i}")
-            return False # One letter cannot be framed by brackets
-        elif ch.isalpha() and stack and last_char in {'(', '['} and stack[-1] == last_char:
-            print(f"Error: One letter cannot be framed by brackets at position {i}")
-            return False # One letter cannot be framed by brackets
-        last_char = ch
-    if stack:
-        print(f"Error: Extra '{stack[-1]}' at position {len(expr)}")
-        return False # Extra opening parenthesis
-    return True
+def CheckExpression(ch):
+    def operator():
+        if index < len(ch) and ch[index] not in "+-*/":
+            return "error"
+
+    def letter():
+        if index < len(ch) and ch[index] not in "abcdefghijklmnopqrstuvwxyz":
+            return "error"
+
+    def KSZ():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] in "([":
+            expression = SZ1()
+            if expression:
+                return expression
+            index += 1
+            expression = operator()
+            if expression:
+                return expression
+            index += 1
+            expression = SZ2()
+            if expression:
+                return expression
+        elif index < len(ch) and ch[index] in "abcdefghijklmnopqrstuvwxyz":
+            expression = letter()
+            if expression:
+                return expression
+            index += 1
+            expression = operator()
+            if expression:
+                return expression
+            index += 1
+            expression = PSZ()
+            if expression:
+                return expression
+        else:
+            return "error"
+
+    def PSZ():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] in "([":
+            expression = SZ1()
+            if expression:
+                return expression
+            index += 1
+            expression = KON2()
+            if expression:
+                return expression
+        elif index < len(ch) and ch[index] in "abcdefghijklmnopqrstuvwxyz":
+            expression = letter()
+            if expression:
+                return expression
+            index += 1
+            expression = KON1()
+            if expression:
+                return expression
+        else:
+            return "error"
+
+    def SZ1():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] == '(':
+            index += 1
+            expression = KSZ()
+            if expression:
+                return expression
+            if index == len(ch) or ch[index] != ')':
+                return "error"
+        elif index < len(ch) and ch[index] == '[':
+            index += 1
+            expression = KSZ()
+            if expression:
+                return expression
+            if index == len(ch) or ch[index] != ']':
+                return "error"
+        else:
+            return "error"
+
+    def SZ2():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] in "([":
+            expression = SZ1()
+            if expression:
+                return expression
+            index += 1
+            expression = KON3()
+            if expression:
+                return expression
+        elif index < len(ch) and ch[index] in "abcdefghijklmnopqrstuvwxyz":
+            expression = letter()
+            if expression:
+                return expression
+            index += 1
+            expression = KON1()
+            if expression:
+                return expression
+        else:
+            return "error"
+
+    def KON1():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] in "+-*/":
+            expression = operator()
+            if expression:
+                return expression
+            index += 1
+            expression = PSZ()
+            if expression:
+                return expression
+
+    def KON2():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] in "+-*/":
+            expression = operator()
+            if expression:
+                return expression
+            index += 1
+            expression = SZ2()
+            if expression:
+                return expression
+
+    def KON3():
+        nonlocal index, expression
+        if index < len(ch) and ch[index] in "+-*/":
+            expression = operator()
+            if expression:
+                return expression
+            index += 1
+            expression = letter()
+            if expression:
+                return expression
+            index += 1
+            expression = KON1()
+            if expression:
+                return expression
+
+    expression = ''
+    index = 0
+    expression = PSZ()
+    if expression or len(ch) != index:
+        return "The entered expression does NOT satisfy the grammar!"
+    else:
+        return "The entered expression satisfies the grammar!"
+
 
 while True:
-    expression = input("Please enter your expression (press 'q' to quit): ")
-    if expression == 'q':
+    expression = input("Enter your expression (or 'exit' to quit): ")
+    if expression == 'exit':
         break
-    if custom_bracket_form(expression):
-        print("The expression is valid.")
+    result = CheckExpression(expression)
+    print(result)
